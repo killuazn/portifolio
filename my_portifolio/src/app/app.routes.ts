aa09a85181327ae8750a7321401aaf0a64401bb1
killuazn <juliomaiscedo@gmail.com>
2025-03-12 17:26:00 -0300

    remove paste

--- my_portifolio/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './shared/pages/home/home.component';
-
-export const routes: Routes = [
-  { path: '**', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent }
-];
